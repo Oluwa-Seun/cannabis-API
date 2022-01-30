dc6077e193f1286e3b168f21dd735fbacdabcb64
JujuBug <61301594+Oluwa-Seun@users.noreply.github.com>
2022-01-30 13:01:40 +0100

    fixed bugs

--- index.js
+++ index.js
@@ -4,69 +4,73 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 
-const canna = [{
-    name: 'weedmaps',
-    address: 'https://weedmaps.com/strains',
-    base: ''
-        //TODO add more website for better scrapping and to get more result (strains)
-}]
+const strains = [{
+        name: 'weedmaps',
+        address: 'https://weedmaps.com/strains',
+        base: 'https://weedmaps.com'
+            //TODO add more website for better scrapping and to get more result (strains)
+    },
+    {
+        name: 'lealy',
+        address: 'https://leafly.com/strains',
+        base: 'https://leafly.com'
+    },
+
+]
 
 const articles = []
 
-canna.forEach(cannabi => {
-    axios.get(cannabi.address)
+strains.forEach(cannabis => {
+    axios.get(cannabis.address)
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
-
             $('a:contains("strains")', html).each(function() {
                 const title = $(this).text()
                 const url = $(this).attr('href')
-
                 articles.push({
                     title,
-                    url: cannabi.base + url,
-                    source: cannabi.name
+                    url: cannabis.base + url,
+                    source: cannabis.name
                 })
             })
 
         })
-})
+        .catch((error) => {
+            console.log(error)
+        })
 
-app.get('/', (req, res) => {
-    res.json('Welcome to my Cannabis web scraper API')
-})
+    app.get('/', (req, res) => {
+        res.json('Welcome to my Cannabis web scraper API')
+    })
 
-app.get('/strains', (req, res) => {
-    res.json(articles)
-})
+    app.get('/strains', (req, res) => {
+        res.json(articles)
 
-// TODO fix for better endpoint reach 
-app.get('/cannabis:cannabisId', (req, res) => {
-    const cannabisId = req.params.cannabisId
+    })
 
-    const cannabiAddress = canna.filter(cannabi => cannabi.name == cannabisId)[0].address
-    const cannabiBase = canna.filter(cannabi => cannabi.name == cannabisId)[0].base
-
-
-    axios.get(cannabiAddress)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const specificArticles = []
-
-            $('a:contains("strains")', html).each(function() {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                specificArticles.push({
-                    title,
-                    url: ncannabiBase + url,
-                    source: cannabisId
+    // TODO fix for better endpoint reach 
+    app.get('/strain/:strainsID', (req, res) => {
+        console.log(req.params.strainsid)
+        const strainsId = req.params.cannabisId
+        const strainsAddress = canna.filter(cannabi => cannabi.name == cannabisId)[0].address
+        const strainsBase = canna.filter(cannabi => cannabi.name == cannabisId)[0].base
+        axios.get(strainsAddress)
+            .then(response => {
+                const html = response.data
+                const $ = cheerio.load(html)
+                const specificArticles = []
+                $('a:contains("strains")', html).each(function() {
+                    const title = $(this).text()
+                    const url = $(this).attr('href')
+                    specificArticles.push({
+                        title,
+                        url: strainsBase + url,
+                        source: strainsId
+                    })
                 })
+                res.json(specificArticles)
             })
-            res.json(specificArticles)
-        }).catch(err => console.log(err))
+    })
 })
-
-
 app.listen(PORT, () => console.log(`SERVER RUNNING ON port: http://localhost:${PORT}`))
